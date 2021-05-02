8ccdf796f929eaea68a21d22441a00e0053b3cb1
Lynx <lynxat0mic02@gmail.com>
2021-05-02 18:24:24 +0100

    updated autoloader to work with linux

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -2,12 +2,12 @@
 spl_autoload_register("autoloader");
 
 function autoloader($className) { // autoload classes from lib, app/controllers, and app/models
-    if (file_exists(ROOT . DS . 'lib' . DS . strtolower($className) . '.class.php')) {
-        require_once(ROOT . DS . 'lib' . DS . strtolower($className) . '.class.php');
-    } else if (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . strtolower($className) . '.php')) {
-        require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . strtolower($className) . '.php');
-    } else if (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . strtolower($className) . '.php')) {
-        require_once(ROOT . DS . 'app' . DS . 'models' . DS . strtolower($className) . '.php');
+    if (file_exists(ROOT . DS . 'lib' . DS . $className . '.class.php')) {
+        require_once(ROOT . DS . 'lib' . DS . $className . '.class.php');
+    } else if (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+    } else if (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
     } else {
 //        die("Could not find class $className");
         Util::show404error();
