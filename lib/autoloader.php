f21a035eda554d7f50ae430a09dc4744297584f9
Lynx <lynxat0mic02@gmail.com>
2021-05-02 19:41:46 +0100

    of course that didn't work, why would it? i'm acc dumb

--- lib/autoloader.php
+++ lib/autoloader.php
@@ -2,6 +2,7 @@
 spl_autoload_register("autoloader");
 
 function autoloader($className) { // autoload classes from lib, app/controllers, and app/models
+    $className = ucwords($className);
     if (file_exists(ROOT . DS . 'lib' . DS . $className . '.class.php')) {
         require_once(ROOT . DS . 'lib' . DS . $className . '.class.php');
     } else if (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
