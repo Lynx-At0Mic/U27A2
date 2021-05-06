cfa217b2b6479dc1df2e2327044c0fa3cdf87f6f
Lynx <lynxat0mic02@gmail.com>
2021-05-06 15:47:52 +0100

    updated 404 error page

--- lib/error_pages/404.php
+++ lib/error_pages/404.php
@@ -1,5 +1,7 @@
 <div class="layoutContainer center">
     <div class="contentContainer roundFull">
-        <h2>Awww man, 404</h2>
+        <h1>404, Page not found</h1>
+        <img src="<?php echo BASE_MEDIA . 'assets/img/buffercat.gif';?>">
+        <h2>Bruh what happened?</h2>
     </div>
 </div>
