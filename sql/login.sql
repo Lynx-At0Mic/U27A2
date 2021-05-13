682f9a54c4ba89e602c04458c98e0c311ce4650e
Lynx <lynxat0mic02@gmail.com>
2021-05-13 15:24:57 +0100

    updated sql

--- sql/login.sql
+++ sql/login.sql
@@ -1,13 +1,15 @@
 create table login
 (
-	account_id int auto_increment primary key,
+	account_id int auto_increment
+		primary key,
+	access_level int default 1 not null,
 	username varchar(128) not null,
 	pwd_hash varchar(255) not null,
 	hash_salt varchar(255) not null,
 	access_token varchar(255) not null,
-	creation_date datetime default CURRENT_TIMESTAMP() null,
+	creation_date datetime default current_timestamp() null,
 	last_login_date datetime null,
 	constraint username
-	unique (username)
+		unique (username)
 );
 
